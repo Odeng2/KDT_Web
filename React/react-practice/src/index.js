c5461eb3c60350effcd8a017aaab92a107894026
HeeSeon <delikind02@gmail.com>
2025-08-13 15:52:29 +0900

    8일차실습

---
+++ React/react-practice/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';    //CSS 파일 가져오기: 클론코딩할때는 App.js 활용
+import App from './App';
+// import reportWebVitals from './reportWebVitals';    //에러 코드. 기본 내장되어 있음. 없어도 괜찮다!
+
+
+// //index.html의 아래 코드와 같은 내용임.
+// ReactDOM.render(<App/>, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
