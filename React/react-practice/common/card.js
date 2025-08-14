b08b0754048c0271eb9e52a4c0cd08860cf5bfe7
HeeSeon <delikind02@gmail.com>
2025-08-14 15:19:22 +0900

    9일차실습

---
+++ React/react-practice/common/card.js
@@ -0,0 +1,14 @@
+function Card (props) {
+    return (
+        <div className="card">
+            <div className="card-head">
+                <img src={props.data.imgURL}/>
+            </div>
+            <div className="card-body">
+                <h1>{props.data.title}</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
