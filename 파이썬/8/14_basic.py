b009426aecae8659de42fe03e96688f19da8a21e
HeeSeon <delikind02@gmail.com>
2025-08-20 16:06:03 +0900

    파이썬첫수업

---
+++ 파이썬/8/14_basic.py
@@ -0,0 +1,20 @@
+## 예제 #1:
+# def greet (name, time_of_day='아침'):    #time_of_day의 기본값: 아침
+#     greetings={
+#         '아침': '좋은 아침입니다',
+#         '점심': '맛있는 점심 드세요.'
+#     }
+#     return f"{greetings[time_of_day]}, {name}님!"
+
+# print(greet('dahan'))
+
+
+# # 예제 #2:
+# text = "Python"
+# first_char = text[0]   #'P'
+# lasg_char = text[-1]    #'n' 음수 인덱스는 문자열 끝에서부터 시작
+# #문자열 슬라이싱
+# substring = text[1:4]
+# print(substring)
+
+
